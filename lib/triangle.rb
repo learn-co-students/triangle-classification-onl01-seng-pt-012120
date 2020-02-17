6fa42feda2c71bc6c9f198bdc96a6b36fd51ba1c
Daniel Sun <danielsun12345@gmail.com>
2020-02-17 22:15:27 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  attr_accessor :length, :width, :height 
+  
+  def initialize(length, width, height)
+    @length = length 
+    @width = width 
+    @height = height 
+  end 
+  
+  def kind
+    if length == width == height 
+      :equilateral
+    elsif @length == @width || @width == @height || @length == @height  
+      :isosceles
+    else
+      :scalene 
+    end 
+  end 
+  
+    class TriangleError < StandardError
+      
+    end 
+  
 end
